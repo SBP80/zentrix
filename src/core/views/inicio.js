135568844ec24242be7667a6e0ad853d0c067f5c
SBP80 <sbpreparaciones@gmail.com>
2026-04-02 01:11:37 +0200

    Crear inicio.js

---
+++ src/core/views/inicio.js
@@ -0,0 +1,10 @@
+export function renderInicio() {
+  return `
+    <div class="page">
+      <h1>Inicio</h1>
+      <div class="card">
+        <p>Base modular creada correctamente.</p>
+      </div>
+    </div>
+  `;
+}
